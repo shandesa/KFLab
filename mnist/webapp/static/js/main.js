5e10b8c34a719f2e03c1d9450a60f4af66c4a81e
Johnu George <johnugeorge109@gmail.com>
2018-06-08 21:58:32 +0530

    Adding style changes

--- mnist/webapp/static/js/main.js
+++ mnist/webapp/static/js/main.js
@@ -1,3 +1,24 @@
+$(function() {
+  
+  // sanity check
+  console.log( "ready!" );
+ 
+  // image click
+  $(".img").click(function() {
+ 
+    //remove all active ones
+    $(".img").removeClass("active");
+    // add active class to clicked picture
+    $(this).addClass("active")
+ 
+    // grab image url
+    var image = $(this).attr("src")
+    console.log(image)
+ 
+  });
+ 
+});
+
 var app = new Vue({
     delimiters:['${', '}'],
     el: '#app',
@@ -26,8 +47,8 @@ var app = new Vue({
         },
         submit: function () {
             //this.inferred_digit = "inferring for " + this.image_to_be_identified + "...",
-            this.show = false,
-            setTimeout(() => this.show = true, 5000)
+            this.show = false;
+            //setTimeout(() => this.show = true, 5000)
 
             $.ajax({
                   context: this,
@@ -38,12 +59,15 @@ var app = new Vue({
                   success: function(result) {
                     this.inferred_digit = result
                     console.log("result"+result);
+                    this.show = true;
                   },
                   // handle error
                   error: function(error) {
                     console.log("Error "+error);
+                    this.show = true;
                   }
              });
+             $(".img").removeClass("active");
         }
     }
 })
