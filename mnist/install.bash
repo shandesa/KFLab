e4e4eb9bde4166df7799ab6597182d5f121b757d
ramdootp <ramdoot@gmail.com>
2018-06-21 09:22:49 -0700

    fix tf operator version, till the issue with latest is resolved

--- mnist/install.bash
+++ mnist/install.bash
@@ -41,6 +41,8 @@ ks pkg install ciscoai/tf-${APP_NAME}job@${CISCOAI_GITHUB_VERSION}
 # kubectl create clusterrolebinding your-user-cluster-admin-binding --clusterrole=cluster-admin --user=<your@email.com>
 
 ks generate kubeflow-core kubeflow-core
+ks param set kubeflow-core tfJobImage "gcr.io/kubeflow-images-public/tf_operator:v20180522-77375baf"
+ks param set kubeflow-core tfJobVersion v1alpha1
 ks apply ${KF_ENV} -c kubeflow-core
 
 #7. Deploy NFS server in the k8s cluster **(Optional step)**
