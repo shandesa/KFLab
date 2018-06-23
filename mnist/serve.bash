3a577fa3c31d48880d8a50839f30f539b4de0738
ramdootp <ramdoot@gmail.com>
2018-06-23 07:17:13 -0700

    updating gke script, separating port forward

--- mnist/serve.bash
+++ mnist/serve.bash
@@ -30,5 +30,5 @@ ks param set tfserving nfsPVC ${NFS_PVC_NAME}
 ks apply ${KF_ENV} -c tfserving
 
 # Port forward to access the serving port locally
-SERVING_POD_NAME=`kubectl -n ${NAMESPACE} get pod -l=app=mnist -o jsonpath='{.items[0].metadata.name}'`
-kubectl -n ${NAMESPACE} port-forward ${SERVING_POD_NAME} 9000:9000 &
+#SERVING_POD_NAME=`kubectl -n ${NAMESPACE} get pod -l=app=mnist -o jsonpath='{.items[0].metadata.name}'`
+#kubectl -n ${NAMESPACE} port-forward ${SERVING_POD_NAME} 9000:9000 &
